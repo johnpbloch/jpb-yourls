96ff070a35bd2399317a508d33ff703406b453dc
John P. Bloch <johnpbloch@gmail.com>
2012-02-19 17:58:30 -0500

    Add a local yourls class.

---
+++ lib/ShortlinkYourlsLocal.php
@@ -0,0 +1,13 @@
+<?php
+
+class ShortlinkYourlsLocal implements Shortlink {
+
+	public static function shorten( $longURL, $desired ) {
+		
+	}
+
+	public static function expand( $shortURL ) {
+		
+	}
+
+}
