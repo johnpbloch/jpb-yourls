28480b821c32f89d38a441ccd3b9ec588413051f
John P. Bloch <johnpbloch@gmail.com>
2012-05-12 09:42:11 -0400

    Options can now be stored as a singleton.

--- lib/JBY_Options.php
+++ lib/JBY_Options.php
@@ -3,8 +3,28 @@
 class JBY_Options extends JPB_Options
 {
 
+	protected $_option_name = 'jpb-yourls-options';
 	public $shortlinkAPI = 'yourls';
 	public $shortlinkURI = '';
+
+	/**
+	 * The singleton instance
+	 * @var JBY_Options
+	 */
+	private static $_instance;
+
+	/**
+	 * Access the singleton instance of this option object.
+	 * @return JBY_Options The singleton
+	 */
+	public static function instance()
+	{
+		if( !(self::$_instance instanceof JBY_Options) )
+		{
+			self::$_instance = new JBY_Options();
+		}
+		return self::$_instance;
+	}
 
 	protected function _install()
 	{
