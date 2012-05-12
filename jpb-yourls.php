af57d73c927809087fe89a4c2b7e942d2339941f
John P. Bloch <johnpbloch@gmail.com>
2012-05-12 15:12:51 -0400

    Add a jpb-yourls file to be used later.

---
+++ jpb-yourls.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+  Plugin Name: YOURLS: WordPress to Twitter
+  Plugin URI: http://planetozh.com/blog/my-projects/yourls-wordpress-to-twitter-a-short-url-plugin/
+  Description: Create short URLs for posts with <a href="http://yourls.org/" title="Your Own URL Shortener">YOURLS</a> (or other services such as bit.ly) and tweet them.
+  Author: Ozh
+  Author URI: http:/ozh.org/
+  Version: 1.5.4
+ */
+
+require( dirname( __FILE__ ) . '/index.php' );
